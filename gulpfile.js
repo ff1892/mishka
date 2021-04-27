9bbe7be1d04e573914c6982278530557c7f7f76e
ff1892 <hitrulein@gmail.com>
2021-04-27 17:10:36 +0300

    добавляет код сборки

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ const sync = require("browser-sync").create();
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
+const terser = require("gulp-terser");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -17,12 +21,12 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
-      // csso()
+      autoprefixer(),
+      csso()
     ]))
-    // .pipe(rename("style.min.css"))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -30,20 +34,99 @@ exports.styles = styles;
 
 // HTML
 
-// const html = () => {
-//   return gulp.src("source/*.html")
-//   .pipe(htmlmin( { collapseWhitespace: true }))
-//   .pipe(gulp.dest("source"))
-// }
+const html = () => {
+  return gulp.src("source/*.html")
+  .pipe(htmlmin( { collapseWhitespace: true }))
+  .pipe(gulp.dest("build"))
+}
 
-// exports.html = html;
+exports.html = html;
+
+//  Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+  .pipe(terser())
+  .pipe(rename("script.min.js"))
+  .pipe(gulp.dest("build/js"))
+  .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+//  Images
+
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.images = optimizeImages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.images = copyImages;
+
+// Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+//  Clean
+
+const clean = () => {
+  return del("build");
+};
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+    "!source/img/icons/*.svg"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"))
+  done()
+}
+
+exports.copy = copy;
+
+// Sprite
+
+const sprite = () => {
+return gulp.src("source/img/icons/*.svg")
+.pipe(svgstore({
+  inlineSvg: true
+}))
+.pipe(rename("sprite.svg"))
+.pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -54,13 +137,50 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
+// Build
+
+const build = gulp.series (
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel (
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
 );
+
+exports.build = build;
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,30 @@
+// Menu variables
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+// Modal variables
+
 let buy = document.querySelectorAll(".buy");
 let overlay = document.querySelector(".overlay");
 let size = document.querySelector(".size");
 let choose = document.querySelector(".size__submit");
 
+// Menu script
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+if (navMain.classList.contains('main-nav--closed')) {
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+} else {
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened');
+}
+});
+
+//  Modal script
 
 for (let button of buy) {
   button.addEventListener("click", function(evt) {
