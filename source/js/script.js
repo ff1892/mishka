1e9e7bfc8e0f38b384d05390748f64e6d68cab5a
ff1892 <hitrulein@gmail.com>
2021-05-02 12:00:38 +0300

    добавляет правки по замечаниям к форме и скриптам

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 // Menu variables
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
 // Modal variables
 
@@ -19,15 +19,15 @@ let slideIndex = 0;
 
 // Menu script
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-if (navMain.classList.contains('main-nav--closed')) {
-  navMain.classList.remove('main-nav--closed');
-  navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+if (navMain.classList.contains("main-nav--closed")) {
+  navMain.classList.remove("main-nav--closed");
+  navMain.classList.add("main-nav--opened");
 } else {
-  navMain.classList.add('main-nav--closed');
-  navMain.classList.remove('main-nav--opened');
+  navMain.classList.add("main-nav--closed");
+  navMain.classList.remove("main-nav--opened");
 }
 });
 
